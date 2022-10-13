164cd740f965358d1a94532c1309d0312cf6af91
leticiaspcruz <leticiaspcruz@gmail.com>
2022-10-12 21:27:06 -0300

    adding tokens at tailwindcss config

--- ignitelab-ds/tailwind.config.cjs
+++ ignitelab-ds/tailwind.config.cjs
@@ -4,6 +4,14 @@ module.exports = {
     '/.src/**/*.tsx',
   ],
   theme: {
+    colors: {
+      'gray': '#5E5E5E',
+      'white': '#FFFFFF',
+      'light': '#D4D4D4',
+      'green': '#64DD17',
+      'green-dark': '#1FAA00',
+      'green-light': '#9CFF57', 
+    },
     extend: {
       fontFamily: { 
         sans: 'Yeseva One, sans-serif'
