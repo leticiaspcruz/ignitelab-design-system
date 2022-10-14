3503121b82683cf222384ac68967ac953cc855f6
leticiaspcruz <leticiaspcruz@gmail.com>
2022-10-14 16:24:06 -0300

    fixing deploy

--- ignitelab-ds/.storybook/main.cjs
+++ ignitelab-ds/.storybook/main.cjs
@@ -15,11 +15,4 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.base = '/ignitelab-design-system/'
-    }
-
-    return config
-  }
 }
